b38cbcf0140b1e14f146b2af6dc2557e997e5a17
Kelly Lampotang <klampotang@fb.com>
2016-06-15 16:18:50 -0700

    Added fade in for images

--- flick/MoviesViewController.swift
+++ flick/MoviesViewController.swift
@@ -11,7 +11,7 @@ import AFNetworking
 import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var movies:[NSDictionary]?
     
@@ -35,18 +35,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue: NSOperationQueue.mainQueue()
         )
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-
-        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
-            completionHandler: { (dataOrNil, response, error) in
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: { (dataOrNil, response, error) in
             if let data = dataOrNil {
-            if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-            data, options:[]) as? NSDictionary {
-                print("response: \(responseDictionary)")
-                self.movies = responseDictionary["results"] as! [NSDictionary]
-                self.tableView.reloadData()
-                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                    data, options:[]) as? NSDictionary {
+                    print("response: \(responseDictionary)")
+                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                    self.tableView.reloadData()
+                    MBProgressHUD.hideHUDForView(self.view, animated: true)
+                }
             }
-          }
         })
         task.resume()
         
@@ -55,7 +54,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -63,12 +62,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let movies = movies {
-           return movies.count
+            return movies.count
         } else {
             return 0
         }
         
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
@@ -83,9 +83,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let baseURL = "http://image.tmdb.org/t/p/w500"
         let posterPath = movie["poster_path"] as! String
         let imageURL = NSURL(string: baseURL+posterPath)
-        
+        cell.posterView.alpha = 0.0
+        UIView.animateWithDuration(2.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            cell.posterView.alpha = 1.0
+            }, completion: nil)
         cell.posterView.setImageWithURL(imageURL!)
-        print("row\(indexPath.row)")
         return cell
     }
     // Makes a network request to get updated data
@@ -109,26 +111,35 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         )
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) in
-                                                                        
-          // ... Use the new data to update the data source ...
-                                                                        
-          // Reload the tableView now that there is new data
-          self.tableView.reloadData()
-                                                                        
-          // Tell the refreshControl to stop spinning
-          refreshControl.endRefreshing()
+            
+            // ... Use the new data to update the data source ...
+            
+            // Reload the tableView now that there is new data
+            self.tableView.reloadData()
+            
+            // Tell the refreshControl to stop spinning
+            refreshControl.endRefreshing()
         });
         task.resume()
     }
-
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+public extension UIView {
+    func fadeIn(duration duration: NSTimeInterval = 1.0) {
+        UIView.animateWithDuration(duration, animations: {
+            self.alpha = 1.0
+        })
     }
-    */
-
 }
