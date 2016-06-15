cefc1462c517dda308e1a98d357adc1032af40ed
Kelly Lampotang <klampotang@fb.com>
2016-06-15 15:06:36 -0700

    added pods

--- flick/MoviesViewController.swift
+++ flick/MoviesViewController.swift
@@ -71,6 +71,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
+        let baseURL = "http://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        let imageURL = NSURL(string: baseURL+posterPath)
         print("row\(indexPath.row)")
         return cell
     }
